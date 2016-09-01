5935bb2023d0ea9de84a47a50dcbdf064c706993
jabari <jabarinuruddin@gmail.com>
2016-09-01 13:57:54 -0400

    removed temporty files

---
+++ Sq1/Painter/Pieces/Sticker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using PuzzleImageGenerator.Shared;
+
+namespace PuzzleImageGenerator.Sq1.Painter.Pieces
+{
+    class Sticker
+    {
+        public CoordPair[] Coords { get; set; }
+
+        public string Color { get; private set; }
+
+        public Sticker(List<CoordPair> coords, string color)
+        {
+            Coords = coords.ToArray();
+            Color = color;
+        }
+    }
+}
